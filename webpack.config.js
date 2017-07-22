a96fb387f087a409d9696a2d9aabb47b03557615
Ma'moun Othman <artofeclipse@gmail.com>
2017-07-22 13:52:26 +0300

    Added .gitignore, project cleanup and styling.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './App/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './App/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
